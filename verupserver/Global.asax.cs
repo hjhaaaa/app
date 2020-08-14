d0e02623eba1faf86df5005c231ae4d6a9446f07
hjh <330699183@qq.com>
2020-08-14 12:51:17 +0800

    ..

--- verupserver/Global.asax.cs
+++ verupserver/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Web.Routing;
 
 namespace verupserver
@@ -11,8 +13,21 @@ namespace verupserver
     {
         protected void Application_Start()
         {
+            #region 返回数据格式化说明
+            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+            var json = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            json.SerializerSettings.DateFormatHandling = Newtonsoft.Json.DateFormatHandling.IsoDateFormat;
+            json.SerializerSettings.DateFormatString = "yyyy-MM-dd HH:mm:ss";
+            json.SerializerSettings.DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Local;
+            json.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+            json.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.None;
+            json.SerializerSettings.ContractResolver = new DefaultContractResolver();
+            #endregion
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+            //GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+            GlobalConfiguration.Configure(o => {
+                o.EnableCors(new EnableCorsAttribute("*","*","*"));
+            });
         }
     }
 }
